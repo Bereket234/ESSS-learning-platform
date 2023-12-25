8bcbebc1ff9962aedb846b379737abc834fabea2
Brook Alemu <brookwworku3@gmail.com>
2023-12-25 10:57:35 -0900

    implemented footer ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const poppins = Poppins({
   subsets: ["latin", "latin-ext", "devanagari"],
@@ -21,11 +22,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-
       <body className={poppins.className}>
         <Navbar />
         {children}
       </body>
+      <Footer />
     </html>
   );
 }
