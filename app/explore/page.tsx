4a9f6a47e1f7a967555e55d7c34751610a33e632
berek47 <106895952+berek47@users.noreply.github.com>
2024-02-08 01:25:08 +0300

    expore static page

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 const Home = () => {
   return (
     <>
-      <div className="h-[151px] bg-Primary mt-0"></div>
       <div className="p-20">
         <div>
           <h1 className="font-SofiaProMedium text-4xl mb-[59px] ">
@@ -12,26 +11,24 @@ const Home = () => {
         </div>
 
         <div>
-          <Link href="/explore/beginner" className="max-w-[1723px] mx-auto">
-            <div className="bg-[url(/assets/images/explore/BeginningPage.png)] flex bg-cover items-center justify-end min-h-[110px] max-w-[1723px] mx-auto lg:min-h-[230px] md:min-h-[180px] sm:min-h-[150px]  border-solid border-4 mb-[130px] rounded-3xl  border-Primary ">
-              {/* <Image src="" alt="Image" className="mx-auto" /> */}
+          <Link href="/explore/Beginner" className="max-w-[1723px] mx-auto">
+            <div className="bg-[url(/assets/images/explore/beginning.png)] flex bg-cover items-center justify-end min-h-[110px] max-w-[1723px] mx-auto lg:min-h-[230px] md:min-h-[180px] sm:min-h-[150px]  border-solid border-4 mb-[130px] rounded-3xl  border-Primary ">
               <h2 className="font-SofiaProMedium text-Primary xl:text-[187px] py-6 mr-4 lg:text-9xl md:text-8xl  sm:text-6xl text-4xl">
                 Beginner
               </h2>
             </div>
           </Link>
 
-          <Link href="/explore/beginner" className="max-w-[1723px] mx-auto">
-            <div className="bg-[url(/assets/images/explore/IntermediatePage.png)] flex bg-cover items-center justify-start min-h-[110px] max-w-[1723px] mx-auto lg:min-h-[230px] md:min-h-[180px] sm:min-h-[150px]  border-solid border-4 mb-[120px] rounded-3xl border-Secondary ">
+          <Link href="/explore/Intermediate" className="max-w-[1723px] mx-auto">
+            <div className="bg-[url(/assets/images/explore/intermediate.png)] flex bg-cover items-center justify-start min-h-[110px] max-w-[1723px] mx-auto lg:min-h-[230px] md:min-h-[180px] sm:min-h-[150px]  border-solid border-4 mb-[120px] rounded-3xl border-Secondary ">
               <h2 className="font-SofiaProMedium text-Secondary  xl:text-[187px] py-6 ml-6 lg:text-9xl md:text-8xl sm:text-6xl text-4xl">
                 Intermediate
               </h2>
             </div>
           </Link>
 
-          <Link href="/explore/beginner" className="max-w-[1723px] mx-auto">
-            <div className="bg-[url(/assets/images/explore/RelativeAdvancedPage.png)] flex bg-cover items-center justify-end min-h-[110px] max-w-[1723px] mx-auto lg:min-h-[230px] md:min-h-[180px] sm:min-h-[150px] border-solid border-4 mb-[318px] rounded-3xl border-Primary">
-              {/* <Image src="" alt="Image" className="mx-auto" /> */}
+          <Link href="/explore/Advanced" className="max-w-[1723px] mx-auto">
+            <div className="bg-[url(/assets/images/explore/advanced.png)] flex bg-cover items-center justify-end min-h-[110px] max-w-[1723px] mx-auto lg:min-h-[230px] md:min-h-[180px] sm:min-h-[150px] border-solid border-4 mb-[318px] rounded-3xl border-Primary">
               <h2 className="font-SofiaProMedium text-Primary  xl:text-[187px] py-6 mr-4 lg:text-9xl md:text-8xl sm:text-6xl text-4xl">
                 Advanced
               </h2>
@@ -39,7 +36,6 @@ const Home = () => {
           </Link>
         </div>
       </div>
-      <div className="h-[477px] bg-Secondary mt-0"></div>
     </>
   );
 };
