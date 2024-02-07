959faacbb1e729ca7556e40ac23f5ce92c98ebf6
joephy527 <joephy527@gmail.com>
2024-02-07 06:52:47 -0800

    globalize sidebar state with reducer

--- store/store.ts
+++ store/store.ts
@@ -1,13 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './slice'
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./slice";
+import sidebarReducer from "./features/sidebarslice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    isSidebar: sidebarReducer,
   },
-})
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
