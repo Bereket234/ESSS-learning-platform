ff9884a1ef970da568c021d97ec5b7300ad3c1bf
berek47 <berekettadessekuche75@gmail.com>
2024-04-23 14:32:48 +0300

    Static explore page update

--- app/explore/[level]/page.tsx
+++ app/explore/[level]/page.tsx
@@ -1,7 +1,43 @@
 import Card from "@/app/components/explore/dynamic-explore/Card";
 
-const Page = ({ params }: { params: { level: string } }) => {
+const DynamicExplorePage = ({ params }: { params: { level: string } }) => {
   const cardData = [
+    {
+      Image: "/assets/images/explore/beginner.png",
+      title: "Space Exploration",
+      description:
+        "Deep dive into the world of space exploration. Get to know the history, missions, and cutting-edge technology...",
+    },
+    {
+      Image: "/assets/images/explore/intermediate.png",
+      title: "Space Engineering",
+      description:
+        "Deep dive into the world of space exploration. Get to know the history, missions, and cutting-edge technology...",
+    },
+    {
+      Image: "/assets/images/explore/advanced.png",
+      title: "Cosmology",
+      description:
+        "Deep dive into the world of space exploration. Get to know the history, missions, and cutting-edge technology...",
+    },
+    {
+      Image: "/assets/images/explore/beginner.png",
+      title: "Space Exploration",
+      description:
+        "Deep dive into the world of space exploration. Get to know the history, missions, and cutting-edge technology...",
+    },
+    {
+      Image: "/assets/images/explore/intermediate.png",
+      title: "Space Engineering",
+      description:
+        "Deep dive into the world of space exploration. Get to know the history, missions, and cutting-edge technology...",
+    },
+    {
+      Image: "/assets/images/explore/advanced.png",
+      title: "Cosmology",
+      description:
+        "Deep dive into the world of space exploration. Get to know the history, missions, and cutting-edge technology...",
+    },
     {
       Image: "/assets/images/explore/beginner.png",
       title: "Space Exploration",
@@ -24,7 +60,7 @@ const Page = ({ params }: { params: { level: string } }) => {
 
   return (
     <>
-      <div className="flex gap-11 mb-[424px] p-8 justify-between ">
+      <div className="flex flex-wrap gap-4 md:gap-8 mb-8 p-4 md:p-8 justify-center">
         {cardData.map((card) => (
           <Card
             key={card.title}
@@ -39,4 +75,4 @@ const Page = ({ params }: { params: { level: string } }) => {
   );
 };
 
-export default Page;
+export default DynamicExplorePage;
