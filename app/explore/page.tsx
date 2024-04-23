eff3cc889f5cf16b6a1fb443cf6026b7563c23cb
berek47 <berekettadessekuche75@gmail.com>
2024-04-23 14:24:24 +0300

    static page update and responsive

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -2,11 +2,7 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-<<<<<<< HEAD
     <div className="pt-10 p-20">
-=======
-    <div className="p-20">
->>>>>>> 23609d0388dd526d2466fec543e02c9b1966068c
       <div>
         <h1 className="font-SofiaProMedium text-4xl mb-[59px] ">
           Explore Courses based Difficulty
@@ -15,26 +11,16 @@ const Home = () => {
 
       <div>
         <Link href="/explore/Beginner" className="max-w-[1723px] mx-auto">
-<<<<<<< HEAD
           <div className="bg-[url(/assets/images/explore/beginning.png)] flex bg-cover items-center justify-end min-h-[110px] max-w-[1723px] mx-auto lg:min-h-[230px] lg:mb-32 md:min-h-[180px] md:mb-16 sm:min-h-[150px] mb-7 border-solid border-4 rounded-3xl  border-Primary ">
             <h2 className="font-SofiaProMedium text-Primary xl:text-[187px] py-6 mr-4 lg:text-9xl md:text-8xl  sm:text-6xl min-[500px]:text-5xl text-3xl">
-=======
-          <div className="bg-[url(/assets/images/explore/beginning.png)] flex bg-cover items-center justify-end min-h-[110px] max-w-[1723px] mx-auto lg:min-h-[230px] md:min-h-[180px] sm:min-h-[150px]  border-solid border-4 mb-[130px] rounded-3xl  border-Primary ">
-            <h2 className="font-SofiaProMedium text-Primary xl:text-[187px] py-6 mr-4 lg:text-9xl md:text-8xl  sm:text-6xl text-4xl">
->>>>>>> 23609d0388dd526d2466fec543e02c9b1966068c
               Beginner
             </h2>
           </div>
         </Link>
 
         <Link href="/explore/Intermediate" className="max-w-[1723px] mx-auto">
-<<<<<<< HEAD
           <div className="bg-[url(/assets/images/explore/intermediate.png)] flex bg-cover items-center justify-start min-h-[110px] max-w-[1723px] mx-auto lg:min-h-[230px] lg:mb-32 md:min-h-[180px] md:mb-16 sm:min-h-[150px] mb-7  border-solid border-4 rounded-3xl border-Secondary ">
             <h2 className="font-SofiaProMedium text-Secondary  xl:text-[187px] py-6 ml-6 lg:text-9xl md:text-8xl sm:text-6xl min-[500px]:text-5xl text-3xl">
-=======
-          <div className="bg-[url(/assets/images/explore/intermediate.png)] flex bg-cover items-center justify-start min-h-[110px] max-w-[1723px] mx-auto lg:min-h-[230px] md:min-h-[180px] sm:min-h-[150px]  border-solid border-4 mb-[120px] rounded-3xl border-Secondary ">
-            <h2 className="font-SofiaProMedium text-Secondary  xl:text-[187px] py-6 ml-6 lg:text-9xl md:text-8xl sm:text-6xl text-4xl">
->>>>>>> 23609d0388dd526d2466fec543e02c9b1966068c
               Intermediate
             </h2>
           </div>
@@ -42,11 +28,7 @@ const Home = () => {
 
         <Link href="/explore/Advanced" className="max-w-[1723px] mx-auto">
           <div className="bg-[url(/assets/images/explore/advanced.png)] flex bg-cover items-center justify-end min-h-[110px] max-w-[1723px] mx-auto lg:min-h-[230px] md:min-h-[180px] sm:min-h-[150px] border-solid border-4 mb-[318px] rounded-3xl border-Primary">
-<<<<<<< HEAD
             <h2 className="font-SofiaProMedium text-Primary  xl:text-[187px] py-6 mr-4 lg:text-9xl md:text-8xl sm:text-6xl min-[500px]:text-5xl text-3xl">
-=======
-            <h2 className="font-SofiaProMedium text-Primary  xl:text-[187px] py-6 mr-4 lg:text-9xl md:text-8xl sm:text-6xl text-4xl">
->>>>>>> 23609d0388dd526d2466fec543e02c9b1966068c
               Advanced
             </h2>
           </div>
