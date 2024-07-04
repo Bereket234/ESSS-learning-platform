ce3df4689c3b285d75934a6b01ff4c8179614c02
joephy527 <joephy527@gmail.com>
2024-07-05 01:59:07 +0300

    configure api for rtk query

--- store/store.ts
+++ store/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slice";
 import sidebarReducer from "./features/sidebarslice";
+import { esssApi } from "./api";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     isSidebar: sidebarReducer,
+    [esssApi.reducerPath]: esssApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(esssApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
