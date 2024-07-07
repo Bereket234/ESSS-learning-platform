423073cd55f1ed4b1af3b69ac9d6aa5b817ea5c6
Nathnael Teshome <nathnaelteshome10@gmail.com>
2024-07-07 17:59:47 +0300

    refactor: Update ExploreSection and TopicCard components with responsive design

--- app/components/home_Page/TopicCard.tsx
+++ app/components/home_Page/TopicCard.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 const TopicCard = ({ title, imageUrl }: CardProps) => {
   return (
     <div
-      className="flex w-[60vw] max-w-[24rem] min-h-[8rem]  md:max-w-none  2 mx-4 h-[80%] md:w-80 md:h-[80%]  lg:h-64 items-end grayscale hover:grayscale-0  transition-all rounded-3xl p-4 bg-cover bg-center text-l hover:text-xl text-white "
+      className="flex w-[60vw] max-w-[24rem] min-h-[8rem] md:max-w-none mx-4 h-[80%] md:w-80 md:h-[80%]  lg:h-64 items-end grayscale hover:grayscale-0  transition-all rounded-3xl p-4 bg-cover bg-center text-l hover:text-xl text-white "
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
       <h2 className="font-semibold italic">{title}</h2>
