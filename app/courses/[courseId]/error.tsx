9ac2d5a2e18da76ed2b07d12bef0c782001eb12e
joephy527 <joephy527@gmail.com>
2024-07-08 17:47:52 +0300

    update error components

--- app/courses/[courseId]/error.tsx
+++ app/courses/[courseId]/error.tsx
@@ -1,7 +1,18 @@
 "use client";
 
-const error = () => {
-  return <div>Error</div>;
+const Error = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
 };
 
-export default error;
+export default Error;
