e7414cde05085933347a1995c5198fe5c6206af7
berek47 <berekettadessekuche75@gmail.com>
2024-07-09 18:22:18 +0300

    updated mobile responsive

--- app/components/courses/ProgressBar.tsx
+++ app/components/courses/ProgressBar.tsx
@@ -10,7 +10,7 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ progress}) => {
 
   return (
     <div className="flex items-center">
-      <div className="sm:max-w-[310px] md:max-w-[440px] lg:max-w-[640px] xl:max-w-[840px] 2xl:max-w-[1040px] w-full bg-Quaternary rounded-full h-[19px] overscroll-none current_course-wrapper">
+      <div className="max-w-[220px] sm:max-w-[310px] md:max-w-[440px] lg:max-w-[640px] xl:max-w-[840px] 2xl:max-w-[1040px] w-full bg-Quaternary rounded-full h-[19px] overscroll-none current_course-wrapper">
         <div
           className="progress-bar bg-Senary h-18 rounded-full"
           style={{ width: `${currentProgress}%`, height: '19px'}}
