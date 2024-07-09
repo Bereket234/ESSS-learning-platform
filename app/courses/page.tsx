b048da9c52cacfd0f92da44599f993ba87bdf80c
berek47 <berekettadessekuche75@gmail.com>
2024-07-09 18:22:18 +0300

    updated mobile responsive

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -13,7 +13,7 @@ const Courses = () => {
         <h1 className='font-SofiaProMedium text-4xl '>Your Courses</h1>
       </div>
       <div className='flex justify-center items-center mb-10'>
-        <div className="min-w-[360px] px-3 sm:min-w-[480px] md:min-w-[640px] lg:min-w-[1020px] xl:min-w-[1390px] pt-6 pb-4 border-2 rounded-[50px] shadow-lg">
+        <div className="max-w-[320px] px-3 sm:min-w-[480px] md:min-w-[640px] lg:min-w-[1020px] xl:min-w-[1390px] pt-6 pb-4 border-2 rounded-[50px] shadow-lg">
           <div className="flex px-5 items-center content-center flex-col sm:flex-col md:flex-col lg:flex-row">
             {/*Course Image */}
             <div className="h-[280px] w-[285px] rounded-4xl overflow-hidden sm:w-[400px] md:h-[500px] md:w-[520px] lg:w-[240px] lg:h-[240px] relative">
@@ -25,12 +25,12 @@ const Courses = () => {
                 className="rounded-3xl pb-2"
               />
             </div>
-            <div className="flex flex-col pl-12 flex-grow">
+            <div className="flex flex-col ml-20 sm:ml-12 flex-grow">
               <h3 className="text-[34px] content-center items-center font-SofiaProMedium mb-2 md:text-6xl">Space Weather</h3>
               <ProgressBar progress={36} />
               <h3 className="text-3xl font-SofiaProMedium mb-4">Your Progress</h3>
               {/* Button */}
-              <button className="ml-auto right-0 bg-Primary text-Secondary font-SofiaProSemiBold py-5 px-28 mt-1 rounded-3xl mr-20 transition duration-300 ease-in-out hover:bg-Secondary hover:text-Primary hover:shadow-md ">
+              <button className="sm:ml-auto right-0 bg-Primary text-Secondary font-SofiaProSemiBold py-5 px-28 mt-1 rounded-3xl mr-20 transition duration-300 ease-in-out hover:bg-Secondary hover:text-Primary hover:shadow-md ">
                 Resume
               </button>
             </div>
@@ -38,7 +38,7 @@ const Courses = () => {
         </div>
       </div>
       <div className='flex justify-center items-center mb-10'>
-        <div className="min-w-[360px] px-3 sm:min-w-[480px] md:min-w-[640px] lg:min-w-[1020px] xl:min-w-[1390px] pt-6 pb-4 border-2 rounded-[50px] shadow-lg">
+        <div className="max-w-[320px] px-3 sm:min-w-[480px] md:min-w-[640px] lg:min-w-[1020px] xl:min-w-[1390px] pt-6 pb-4 border-2 rounded-[50px] shadow-lg">
           <div className="flex px-5 items-center content-center flex-col sm:flex-col md:flex-col lg:flex-row">
             <div className="h-[280px] w-[285px] rounded-4xl overflow-hidden sm:w-[400px] md:h-[500px] md:w-[520px] lg:w-[240px] lg:h-[240px] relative">
               {/*Course Image */}
@@ -50,12 +50,12 @@ const Courses = () => {
                   className="rounded-3xl pb-2"
               />
             </div>
-            <div className="flex flex-col ml-12 flex-grow">
+            <div className="flex flex-col ml-20 sm:ml-12 flex-grow">
               <h3 className="text-[34px] content-center items-center font-SofiaProMedium mb-2 md:text-6xl">Aerospace Eng.</h3>
               <ProgressBar progress={72} />
               <h3 className="text-3xl font-SofiaProMedium mb-4">Your Progress</h3>
               {/* Button */}
-              <button className="ml-auto right-0 bg-Primary text-Secondary font-SofiaProSemiBold py-5 px-28 mt-1 rounded-3xl mr-20 transition duration-300 ease-in-out hover:bg-Secondary hover:text-Primary hover:shadow-md ">
+              <button className="sm:ml-auto right-0 bg-Primary text-Secondary font-SofiaProSemiBold py-5 px-28 mt-1 rounded-3xl mr-20 transition duration-300 ease-in-out hover:bg-Secondary hover:text-Primary hover:shadow-md ">
                 Resume
               </button>
             </div>
