ddd7c183875f8d4c20accdd728bfde7c200e4d83
joephy527 <joephy527@gmail.com>
2024-07-11 15:12:11 +0300

    make navbar responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,10 @@
 "use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+
 import { TbWorld } from "react-icons/tb";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoMdClose } from "react-icons/io";
@@ -24,7 +26,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="relative flex container h-18 w-full items-center mx-auto my-3 align-baseline px-5">
+    <nav className="relative flex container h-18 w-full items-center mx-auto my-3 align-baseline px-2 sm:px-5">
       {/* Logo */}
       <div className="flex justify-center align-baseline">
         <div className="flex item-center">
@@ -33,23 +35,23 @@ export default function Navbar() {
             alt="logo"
             width={80}
             height={50}
-            className="object-contain"
+            className="object-contain w-16 h-10 sm:w-20"
           />
         </div>
-        <div className="flex item-center pl-5">
+        <div className="flex item-center pl-2 sm:pl-5">
           <Image
             src="/assets/images/learning.png"
             height={80}
             width={200}
             alt="logo"
-            className="object-contain"
+            className="object-contain w-40 sm:w-[200px]"
           />
         </div>
       </div>
 
       {/* Desktop Navigation Links  */}
       <div className="flex container justify-between">
-        <div className="hidden md:flex space-x-6 pl-6 lg:pl-14 pr-5">
+        <div className="hidden md:flex space-x-6 mx-auto pr-5">
           {links.map((link) => (
             <Link
               key={link.href}
@@ -64,10 +66,10 @@ export default function Navbar() {
             </Link>
           ))}
         </div>
-        <div className="flex space-x-6 items-center">
+        <div className="flex space-x-3 items-center">
           <Link
             href={"/login"}
-            className="hidden text font-bold text-Primary md:block hover:bg-Secondary rounded-full px-2 py-1 lg:text-xl"
+            className="hidden text font-bold text-Primary md:block hover:bg-Secondary rounded-full px-3 py-1 lg:text-xl"
           >
             Login
           </Link>
@@ -82,16 +84,16 @@ export default function Navbar() {
 
         {/* Mobile Navigation Links and Hamburger Menu  */}
         <div className="flex justify-between md:hidden">
-          <div className="flex space-x-4 pr-4">
+          <div className="flex space-x-2 pr-4">
             <Link
               href={"/login"}
-              className=" text font-bold text-Primary hover:bg-Secondary px-2 py-1 rounded-full lg:text-xl"
+              className=" text text-sm sm:text-base font-bold text-Primary hover:bg-Secondary px-2 py-1 rounded-full lg:text-xl"
             >
               Login
             </Link>
             <Link
               href="/register"
-              className=" text font-bold text-Secondary bg-Primary rounded-full px-2 py-1 baseline hover:bg-Secondary hover:text-Primary text-sm"
+              className=" text font-bold sm:text-base text-Secondary bg-Primary rounded-full px-2 py-1 baseline hover:bg-Secondary hover:text-Primary text-sm"
             >
               Sign Up
             </Link>
