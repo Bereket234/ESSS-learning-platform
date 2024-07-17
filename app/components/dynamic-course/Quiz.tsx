e8b3c651c68fe8d5883d8e26389dcf7bd5c72b39
joephy527 <joephy527@gmail.com>
2024-07-17 14:18:54 +0300

    Add quiz logic

--- app/components/dynamic-course/Quiz.tsx
+++ app/components/dynamic-course/Quiz.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { ChangeEvent, FormEvent, useState } from "react";
+
 import { questionsType } from "@/types/dynamic-courses/types";
 
 const Quiz = ({
@@ -7,32 +11,62 @@ const Quiz = ({
   title: string;
   questions: questionsType[];
 }) => {
+  const q = questions.map((question) => {
+    if (!!question.choices) {
+      return question.choices[0];
+    }
+
+    return {};
+  });
+  const [answers, setAnswers] = useState(q);
+
+  const handleChoiceChange = (answer: string, questionId: number) => {
+    answers[questionId] = answer;
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(answers);
+  };
+
   return (
     <div className="text-Primary rounded-[10px] border-2 bg-white px-2 sm:px-12 py-7">
       <h3 className="text-xl font-SofiaProRegular mb-10">Challenge: {title}</h3>
 
-      <ul>
+      <form onSubmit={handleSubmit} className="flex flex-col">
         {questions.map((question, idx) => (
-          <li key={question.key} className="mb-3.5">
-            <p className="font-SofiaProLight mb-0.5">
+          <fieldset key={question.key} className="mb-3.5">
+            <legend className="font-SofiaProLight mb-0.5">
               {idx + 1}. {question.question}
-            </p>
+            </legend>
 
-            <ul className="px-3.5 font-SofiaProLight">
+            <ul className="font-SofiaProLight px-3.5">
               {question.choices.map((choice, i) => (
                 <li key={choice.key} className="flex items-center gap-2">
                   <input
                     type="radio"
                     name={question.question}
                     value={choice.choice}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                      handleChoiceChange(e.target.value, idx)
+                    }
                   />
-                  {String.fromCharCode(97 + i)}) {choice.choice}
+                  <label>
+                    {String.fromCharCode(97 + i)}) {choice.choice}
+                  </label>
                 </li>
               ))}
             </ul>
-          </li>
+          </fieldset>
         ))}
-      </ul>
+
+        <input
+          type="reset"
+          value="Clear all answers"
+          className="p-2 mt-5 self-end rounded-lg text-Secondary bg-Primary border border-Primary hover:text-Primary cursor-pointer font-SofiaProSemiBold hover:bg-Secondary"
+        />
+        <button type="submit">Submit answers</button>
+      </form>
     </div>
   );
 };
